f4957965839ffa2d6a7b9161b630665c6be9ebc6
ninkuk <ninadk03@gmail.com>
2020-06-12 16:45:40 -0700

    Dynamic commands enabled
    Signed-off-by: ninkuk <ninadk03@gmail.com>

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
-// require the discord.js module
+const fs = require('fs');
 const Discord = require('discord.js');
 const {prefix, token} = require('./config.json');
 
-// create a new Discord client
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
-// when the client is ready, run this code
-// this event will only trigger one time after logging in
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+}
+
 client.once('ready', () => {
     console.log('Ready!');
 });
 
 client.on('message', message => {
-    if (message.content === '!bruh') {
-        // send back "Pong." to the channel the message was sent in
-        message.channel.send('**BRUH**');
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).split(/ +/);
+    console.log(args);
+    const command = args.shift().toLowerCase();
+    console.log(command);
+
+    if (!client.commands.has(command)) return;
+
+    try {
+        client.commands.get(command).execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
     }
 });
 
