7cc35b0718a7172921822c5a83b1e87cc6a45e10
ninkuk <ninadk03@gmail.com>
2020-06-12 18:27:15 -0700

    Implemented Bruh emoji response command
    Signed-off-by: ninkuk <ninadk03@gmail.com>

---
+++ commands/bruh.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'bruh',
+    description: 'Reacts with BRUH emojis',
+    execute(message, args) {
+        message.channel.send(":b:");
+        message.channel.send(":regional_indicator_r:");
+        message.channel.send(":regional_indicator_u:");
+        message.channel.send(":regional_indicator_h:");
+    },
+};
