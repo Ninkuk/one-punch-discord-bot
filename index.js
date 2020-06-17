83b0b5604ef742d56f191ae8b799729080b96963
ninkuk <ninadk03@gmail.com>
2020-06-16 18:13:05 -0700

    Implemented the 'k' reply
    Signed-off-by: ninkuk <ninadk03@gmail.com>

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
+    if (message.content === 'k') {
+        message.channel.messages.fetch({limit: 2}).then(message1 => {
+            let author = message1.last().author;
+            message.channel.send(`${author} your messages have been ignored by ${message.author}`);
+        })
+    }
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).split(/ +/);
     console.log(args);
