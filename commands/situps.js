78ac5486f7a6b621836563d3d7d342ccfd33ccbb
Ninad Kulkarni <ninadk03@gmail.com>
2020-08-20 05:24:20 -0700

    Some cosmetic changes to dynamically created commands
    Signed-off-by: ninkuk <ninadk03@gmail.com>

--- commands/situps.js
+++ commands/situps.js
@@ -1,24 +1,24 @@
 module.exports = {
-            name: 'situps',
-            description: 'Enter your daily situps',
-                format: '[# of situps]',
-                    execute(message, args, client) {
-                        const Discord = require('discord.js');
-                        message.delete();
-                
-                        const messageEmbed = new Discord.MessageEmbed()
-                            .setColor("F44336")
-                            .setTitle(`${message.author.username} Achievement`)
-                            .addFields(
-                                {
-                                    name: 'situps',
-                                    value: args[0]
-                                }
-                            )
-                            .setThumbnail("https://mfiles.alphacoders.com/593/593024.png")
-                            .setTimestamp()
-                
-                        const channel = client.channels.cache.get('719502252297158687');
-                        channel.send(messageEmbed);
-                    },
-                };
+    name: 'situps',
+    description: 'Enter your daily situps',
+    format: '[# of situps]',
+    execute(message, args, client) {
+        const Discord = require('discord.js');
+        message.delete();
+
+        const messageEmbed = new Discord.MessageEmbed()
+            .setColor("F44336")
+            .setTitle(`${message.author.username} Achievement`)
+            .addFields(
+                {
+                    name: 'situps',
+                    value: args[0]
+                }
+            )
+            .setThumbnail("https://mfiles.alphacoders.com/593/593024.png")
+            .setTimestamp()
+
+        const channel = client.channels.cache.get('719502252297158687');
+        channel.send(messageEmbed);
+    },
+};
--- commands/skips.js
+++ commands/skips.js
@@ -1,24 +1,24 @@
 module.exports = {
-            name: 'skips',
-            description: 'Enter your daily skips',
-                format: '[# of skips]',
-                    execute(message, args, client) {
-                        const Discord = require('discord.js');
-                        message.delete();
-                
-                        const messageEmbed = new Discord.MessageEmbed()
-                            .setColor("F44336")
-                            .setTitle(`${message.author.username} Achievement`)
-                            .addFields(
-                                {
-                                    name: 'skips',
-                                    value: args[0]
-                                }
-                            )
-                            .setThumbnail("https://mfiles.alphacoders.com/593/593024.png")
-                            .setTimestamp()
-                
-                        const channel = client.channels.cache.get('719502252297158687');
-                        channel.send(messageEmbed);
-                    },
-                };
+    name: 'skips',
+    description: 'Enter your daily skips',
+    format: '[# of skips]',
+    execute(message, args, client) {
+        const Discord = require('discord.js');
+        message.delete();
+
+        const messageEmbed = new Discord.MessageEmbed()
+            .setColor("F44336")
+            .setTitle(`${message.author.username} Achievement`)
+            .addFields(
+                {
+                    name: 'skips',
+                    value: args[0]
+                }
+            )
+            .setThumbnail("https://mfiles.alphacoders.com/593/593024.png")
+            .setTimestamp()
+
+        const channel = client.channels.cache.get('719502252297158687');
+        channel.send(messageEmbed);
+    },
+};
