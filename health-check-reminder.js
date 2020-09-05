2f980ed690586927a0b7448a1018825d0c439d18
Ninad Kulkarni <ninadk03@gmail.com>
2020-09-05 16:21:25 -0700

    hard coded channel id
    Signed-off-by: ninkuk <ninadk03@gmail.com>

--- health-check-reminder.js
+++ health-check-reminder.js
@@ -16,8 +16,8 @@ client.login(token).then(() => {
             .setThumbnail("https://brandguide.asu.edu/sites/default/files/styles/downloadable_images/public/asu_sunburst_rgb_maroongold_150ppi_0.png?itok=oZA6h1Z6")
             .setTimestamp()
 
-        const channel = client.channels.cache.get('720151848136474627')
-        // const channel = client.channels.cache.get('695910739655000108');
+        // const channel = client.channels.cache.get('720151848136474627')
+        const channel = client.channels.cache.get('695910739655000108');
         channel.send('@everyone')
         channel.send(messageEmbed).then(() => {
             client.destroy();
